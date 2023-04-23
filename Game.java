9689d4864c5f4d4bb22c606ee61fa81b41e027fc
tuckers15 <104844788+tuckers15@users.noreply.github.com>
2023-04-23 17:21:32 -0400

    added getters

--- Game.java
+++ Game.java
@@ -7,24 +7,95 @@ public class Game{
     private int deaths;
     private int assists;
     private int kda;
+    private int lp;
     private boolean win;
     private Date playDate;
 
     //constructor
-    public Game(String c, int k, int d, int a, boolean w, Date date){
-        champion = c;
-        kills = k;
-        deaths = d;
-        assists = a;
-        kda = (k+a)/d;
-        win = w;
+    public Game(String champion, int kills, int deaths, int assists, boolean win, Date date){
+        this.champion = champion;
+        this.kills = kills;
+        this.deaths = deaths;
+        this.assists = assists;
+        kda = (kills+assists)/deaths;
+        this.win = win;
         playDate = new Date(date.getTime());
+        lp = getLP(win, kills, assists, deaths);
 
     }
 
+    
+    /** 
+     * @param win
+     * @param kills
+     * @param assists
+     * @param deaths
+     * @return int
+     */
     public int getLP(boolean win, int kills, int assists, int deaths){
-        int lp = 0;
+        int rankedPoints = 0;
 
-        return lp;
+
+        return rankedPoints;
+    }
+
+    
+    /** 
+     * function --> getChampion() 
+     * @return String
+     */
+    public String getChampion(){
+        return champion;
+    }
+
+    
+   /** 
+     * function --> getKills() 
+     * @return int
+    */
+    public int getKills(){
+        return kills;
+    }
+     /** 
+     * function --> getDeaths() 
+     * @return int
+     */
+    public int getDeaths(){
+        return deaths;
+    }
+     /** 
+     * function --> getAssists() 
+     * @return int
+     */
+    public int getAssists(){
+        return assists;
+    }
+     /** 
+     * function --> getKDA()
+     * @return int
+     */
+    public int getKda(){
+        return kda;
+    }
+     /** 
+     * function --> getPlayDate()
+     * @return int
+     */
+    public Date getPlayDate(){
+        return playDate;
+    }
+
+    @Override
+    public String toString(){
+        String s = "";
+        s += "Champion:" + champion + "\n";
+        s += "Kills:" + kills + "\n";
+        s += "Deaths:" + deaths + "\n";
+        s += "Assists:" + assists + "\n";
+        s += "KDA:" + kda + "\n";
+        s += "Win:" + win + "\n";
+        s += "Date:" + playDate + "\n";
+        s += "LP:" + lp + "\n";
+        return s;
     }
 }
--- gameDriver.java
+++ gameDriver.java
@@ -1,8 +1,13 @@
+import java.util.Date;
 public class gameDriver {
 
     public static void main(String [] args)
     {
-        
+        Game g1 = new Game("me", 5, 5, 5, false, new Date());
+
+        g1.getChampion();
+
+        System.out.println(g1);
     }
     
 }
