d01907318ad4c574298551511690a575f426678b
tuckers15 <104844788+tuckers15@users.noreply.github.com>
2023-04-23 16:37:21 -0400

    started constructor and getLP function

--- Game.java
+++ Game.java
@@ -1,7 +1,30 @@
+import java.util.Date;
 public class Game{
-    
+
+    //declaring variables
     private String champion;
     private int kills;
     private int deaths;
+    private int assists;
+    private int kda;
+    private boolean win;
+    private Date playDate;
 
+    //constructor
+    public Game(String c, int k, int d, int a, boolean w, Date date){
+        champion = c;
+        kills = k;
+        deaths = d;
+        assists = a;
+        kda = (k+a)/d;
+        win = w;
+        playDate = new Date(date.getTime());
+
+    }
+
+    public int getLP(boolean win, int kills, int assists, int deaths){
+        int lp = 0;
+
+        return lp;
+    }
 }
---
+++ gameDriver.java
@@ -0,0 +1,8 @@
+public class gameDriver {
+
+    public static void main(String [] args)
+    {
+        
+    }
+    
+}
